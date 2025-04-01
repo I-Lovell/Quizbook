8ecf11a459945f44fd8c7f5a2f1314221b1448f2
Etienne <etilegoater@gmail.com>
2025-04-01 16:56:53 +0100

    Updated the loggedinHeader so that the logout button can be called in the header

--- frontend/src/components/Post/LoggedInHeader.jsx
+++ frontend/src/components/Post/LoggedInHeader.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 import logo from "/Quizler-Logo.png";
 
-export const LoggedInHeader = () => {
+export const LoggedInHeader = ({ onLogout }) => {
   return (
-        <header className="header">
-          <div className="logo-container">
-            <img src={logo} alt="Quizler Logo" className="logo" />
-            <h1 className="title">QuizBook</h1>
-          </div>
-          <nav className="nav">
-            <Link to="/posts" className="nav-button">Home</Link>
-            <Link to="/login" className="nav-button">Logout</Link>
-          </nav>
-        </header>
+    <header className="header">
+      <div className="logo-container">
+        <img src={logo} alt="Quizler Logo" className="logo" />
+        <h1 className="title">QuizBook</h1>
+      </div>
+      <nav className="nav">
+        <Link to="/posts" className="nav-button">Home</Link>
+        <button onClick={onLogout} className="nav-button">Logout</button>
+      </nav>
+    </header>
   );
 }
 
