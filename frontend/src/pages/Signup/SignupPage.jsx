dce4b6547149b36b0fe04f165f01b063ea7a6fad
Emily Sadler <emily@sadler.gg>
2025-04-02 14:42:11 +0100

    added username to signuppage.jsx

--- frontend/src/pages/Signup/SignupPage.jsx
+++ frontend/src/pages/Signup/SignupPage.jsx
@@ -8,12 +8,13 @@ import CreateAccount from "../../components/Post/CreateAccount";
 export const SignupPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await signup(email, password);
+      await signup(email, password, username);
       console.log("redirecting...:");
       navigate("/login");
     } catch (err) {
@@ -28,6 +29,10 @@ export const SignupPage = () => {
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
   };
 
 return (
