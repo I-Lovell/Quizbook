1c6b903d799cc9290776815ff09332806e89f0f4
Luke Howeth <LukeHoweth30@gmail.com>
2025-04-04 12:03:12 +0100

    Add more info to the returned jsons from GET /posts (username, comments, numOfLikes)

--- api/src/controllers/posts.go
+++ api/src/controllers/posts.go
@@ -9,11 +9,20 @@ import (
 	"github.com/makersacademy/go-react-acebook-template/api/src/models"
 )
 
+type PostCommentJSON struct {
+	UserID   uint   `json:"userID"`
+	Username string `json:"username"`
+	Contents string `json:"contents"`
+}
+
 type JSONPost struct {
-	ID       uint   `json:"_id"`
-	Question string `json:"question"`
-	Answer   string `json:"answer"`
-	UserID   uint   `json:"user_id"`
+	ID         uint              `json:"_id"`
+	Question   string            `json:"question"`
+	Answer     string            `json:"answer"`
+	UserID     uint              `json:"user_id"`
+	Username   string            `json:"username"`
+	Comments   []PostCommentJSON `json:"comments"`
+	NumOfLikes int               `json:"numOfLikes"`
 }
 
 func GetAllPosts(ctx *gin.Context) {
@@ -31,11 +40,55 @@ func GetAllPosts(ctx *gin.Context) {
 	// Convert posts to JSON Structs
 	jsonPosts := make([]JSONPost, 0)
 	for _, post := range *posts {
+		// Fetch the post author's username
+		author, err := models.FindUser(strconv.Itoa(int(post.UserID)))
+		authorUsername := "Unknown" // Default if author not found, although this shouldn't happen
+		if err == nil {
+			authorUsername = author.Username
+		}
+		// Consider logging error if err != nil
+
+		// Fetch comments for the post
+		comments, err := models.FetchCommentsByPostID(post.ID)
+		if err != nil {
+			// I think this is probably the best way to hangle this? maybe continue to next post?
+			// For now, we'll just send an internal error for simplicity.
+			SendInternalError(ctx, err)
+			return
+		}
+
+		jsonComments := make([]PostCommentJSON, 0)
+		for _, comment := range *comments {
+			// Find the user who made the comment to get their username
+			user, err := models.FindUser(strconv.Itoa(int(comment.UserID)))
+			username := "Unknown" // Again, this shouldn't happen, but just in case
+			if err == nil {
+				username = user.Username
+			}
+
+			jsonComments = append(jsonComments, PostCommentJSON{
+				UserID:   comment.UserID,
+				Username: username,
+				Contents: comment.Content,
+			})
+		}
+
+		// Fetch likes for the post
+		likes, err := models.FetchLikesByPostID(post.ID)
+		if err != nil {
+			SendInternalError(ctx, err)
+			return
+		}
+		numOfLikes := len(*likes)
+
 		jsonPosts = append(jsonPosts, JSONPost{
-			ID:       post.ID,
-			Question: post.Question,
-			Answer:   post.Answer,
-			UserID:   post.UserID,
+			ID:         post.ID,
+			Question:   post.Question,
+			Answer:     post.Answer,
+			UserID:     post.UserID,
+			Username:   authorUsername,
+			Comments:   jsonComments,
+			NumOfLikes: numOfLikes,
 		})
 	}
 
@@ -98,23 +151,23 @@ func CreatePost(ctx *gin.Context) {
 func GetPostsByUserID(ctx *gin.Context) {
 	userIDParam := ctx.Param("id")
 	userID, err := strconv.ParseUint(userIDParam, 10, 32)
-	
+
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Invalid user ID"})
 		return
 	}
-	
+
 	posts, err := models.FetchPostsByUserID(uint(userID))
-	
+
 	if err != nil {
 		SendInternalError(ctx, err)
 		return
 	}
-	
+
 	val, _ := ctx.Get("userID")
 	tokenUserID := val.(string)
 	token, _ := auth.GenerateToken(tokenUserID)
-	
+
 	// Convert posts to JSON Structs
 	jsonPosts := make([]JSONPost, 0)
 	for _, post := range *posts {
@@ -125,6 +178,6 @@ func GetPostsByUserID(ctx *gin.Context) {
 			UserID:   post.UserID,
 		})
 	}
-	
+
 	ctx.JSON(http.StatusOK, gin.H{"posts": jsonPosts, "token": token})
 }
