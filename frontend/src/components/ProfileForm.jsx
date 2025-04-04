e709f36f4691c269ef97a6f87276d00a2ebc5d3b
Emily Sadler <emily@sadler.gg>
2025-04-04 11:52:18 +0100

    change to profile

---
+++ frontend/src/components/ProfileForm.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { updateProfile } from "../services/profile";
+import { useForm, Controller } from "react-hook-form";
+
+const ProfileForm = () => {
+  const { currentUser } = useCurrentUser();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    getValues,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await updateProfile(data);
+    } catch (error) {
+      console.error("Error updating profile:", error);
+      setError("root", {
+        message: "Failed to update profile. Please try again.",
+      });
+    }
+  };
+
+  return (
+    <div>
+      <form className="profile-form" onSubmit={handleSubmit(onSubmit)}>
+        {errors.root && <span className="error">{errors.root.message}</span>}
+        <div className="profile-picture-container">
+          <Controller
+            name="profilePicture"
+            control={control}
+            render={({ field }) => (
+              <div className="profile-picture-wrapper">
+                <img
+                  src={
+                    field.value ||
+                    "https://img.freepik.com/free-psd/cartoon-question-mark-isolated_23-2151568563.jpg?semt=ais_hybrid"
+                  }
+                  alt="Profile"
+                  className="profile-picture"
+                />
+
+                <div className="profile-picture-overlay">UPDATE</div>
+                <input
+                  type="file"
+                  accept="image/*"
+                  className="profile-picture-input"
+                  onChange={(event) => {
+                    const file = event.target.files[0];
+                    if (file) {
+                      const reader = new FileReader();
+                      reader.readAsDataURL(file);
+                      reader.onloadend = () => {
+                        field.onChange(reader.result);
+                      };
+                    }
+                  }}
+                />
+              </div>
+            )}
+          />
+        </div>
+        <textarea name="bio" className="bio-edit" {...register("bio")} />
+        <button
+          disabled={isSubmitting}
+          role="submit-button"
+          id="submit"
+          type="submit"
+        >
+          {isSubmitting ? "Saving changes..." : "Save changes"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ProfileForm;
--- frontend/src/pages/Profile/ProfilePage.jsx
+++ frontend/src/pages/Profile/ProfilePage.jsx
@@ -1,72 +1,21 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import LoggedInHeader from "../../components/Post/LoggedInHeader";
-import ProfilePicture from "../../components/Post/ProfilePicture";
-import Bio from "../../components/Post/Bio";
 import "./ProfilePage.css";
 import "../Background.css";
-import { getSelf } from "../../services/profile";
-import { useNavigate } from "react-router-dom";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import ProfileForm from "../../components/ProfileForm";
+import ProfileDetails from "../../components/ProfileDetails";
 
 export const ProfilePage = () => {
-  const { currentUser } = useCurrentUser();
-  const navigate = useNavigate();
-  const formRef = useRef(null);
-  const [profilePicture, setProfilePicture] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) return navigate("/login");
-
-    getSelf(token)
-      .then((data) => {
-        setUser(data.user);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
-
-  const updateUser = async (e) => {
-    e.preventDefault();
-
-    const formData = new FormData();
-    formData.append("profilePicture", profilePicture);
-
-    console.log("Profile picture", formData);
-  };
 
   return (
     <div className="home">
       <div className="backim"></div>
       <LoggedInHeader />
-      <form
-        ref={formRef}
-        id="user-form"
-        className="profile-container"
-        onSubmit={(e) => updateUser(e)}
-      >
-        <ProfilePicture
-          defaultValue={profilePicture}
-          onChange={(e) => setProfilePicture(e.target.value)}
-          name="profile-picture"
-          isEditing={isEditing}
-        />
-        <Bio isEditing={isEditing} />
-      </form>
-
       {isEditing ? (
-        <button type="submit" form="user-form" className="edit-profile-button">
-          Save Changes
-        </button>
+        <ProfileForm />
       ) : (
-        <button
-          className="edit-profile-button"
-          onClick={() => setIsEditing(!isEditing)}
-        >
-          Edit My Profile
-        </button>
+        <ProfileDetails toggleEdit={() => setIsEditing(true)} />
       )}
     </div>
   );
