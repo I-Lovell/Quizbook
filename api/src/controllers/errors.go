c67a85cf99d202aeea1b37b3db06ae08bf4149a9
ImogenLovell <imogen.lovell@yahoo.co.uk>
2025-04-07 21:55:25 +0100

    put error handling logic into it's own function in errors.go

--- api/src/controllers/errors.go
+++ api/src/controllers/errors.go
@@ -2,7 +2,7 @@ package controllers
 
 import (
 	"net/http"
-
+	"strings"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,5 +11,19 @@ func SendInternalError(ctx *gin.Context, err error) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"err": "Something went wrong"})
 	} else {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+	}
+}
+
+// ======= Checks if username, email or password are already taken when user signs up ============
+
+func CheckForDuplicateKeyError(ctx *gin.Context, err error) {
+	if strings.Contains(err.Error(), "username") {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Username taken by other user"})
+	} else if strings.Contains(err.Error(), "email") {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Email taken by other user"})
+	} else if strings.Contains(err.Error(), "password") {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message":"Password taken by other user"})
+	} else {
+		SendInternalError(ctx, err)
 	}
 }
--- api/src/controllers/users.go
+++ api/src/controllers/users.go
@@ -35,15 +35,7 @@ func CreateUser(ctx *gin.Context) {
 	// ============================= Save the user to the database ================================
 	_, err = newUser.Save()
 	if err != nil {
-		if strings.Contains(err.Error(), "username") {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Username taken by other user"})
-		} else if strings.Contains(err.Error(), "email") {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Email taken by other user"})
-		} else if strings.Contains(err.Error(), "password") {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"message":"Password taken by other user"})
-		} else {
-			SendInternalError(ctx, err)
-		}
+		CheckForDuplicateKeyError(ctx, err)
 		return
 	}
 
