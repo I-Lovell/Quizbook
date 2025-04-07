cef716aeb2291c48a62232fb2664ea88efce4cca
abbiefinlayson1 <abbiefinlayson@monzo.com>
2025-04-07 16:11:14 +0100

    created deleteuser func in model

--- api/src/models/user.go
+++ api/src/models/user.go
@@ -69,3 +69,19 @@ func UpdateUser(id uint, updates map[string]interface{}) (*User, error) {
 
 	return &user, nil
 }
+
+func Deleteuser (id uint) error {
+	var user User
+
+	// First find the user
+	if err := Database.First(&user, id).Error; err != nil {
+		return err
+	}
+
+	// Attempt to delete the user from the database
+	if err := Database.Delete(&user).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
