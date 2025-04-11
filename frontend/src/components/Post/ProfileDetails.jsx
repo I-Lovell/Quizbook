8a819c95504f040a7e483add6953f9ee64b5e611
Emily Sadler <emily@sadler.gg>
2025-04-11 09:47:36 +0100

    Styling for all buttons to be uniform

--- frontend/src/components/Post/ProfileDetails.jsx
+++ frontend/src/components/Post/ProfileDetails.jsx
@@ -6,25 +6,23 @@ const ProfileDetails = ({ toggleEdit }) => {
   return (
     <div>
       <div className="profile-picture-container">
-      <div className="profilePicture">
-      <img
-        src={
-          currentUser?.profilePicture ||
-          "https://img.freepik.com/free-psd/cartoon-question-mark-isolated_23-2151568563.jpg?semt=ais_hybrid"
-        }
-        alt="Profile"
-        className="profile-picture"
-      />
+        <div className="profilePicture">
+          <img
+            src={
+              currentUser?.profilePicture ||
+              "https://img.freepik.com/free-psd/cartoon-question-mark-isolated_23-2151568563.jpg?semt=ais_hybrid"
+            }
+            alt="Profile"
+            className="profile-picture"
+          />
+        </div>
       </div>
-      </div>
-      <h2 className="username">
-        {currentUser?.username || "No username"}
-      </h2>
+      <h2 className="username">{currentUser?.username || "No username"}</h2>
       <p className="bio">
         {currentUser?.bio || "This is a sample bio. You can add your own bio."}
       </p>
-      <button className="edit-profile-button" onClick={() => toggleEdit(true)}>
-        Edit bio or profile picture
+      <button className="like-button" onClick={() => toggleEdit(true)}>
+        Edit Profile
       </button>
     </div>
   );
